fe009fbc59708bd4456f96eadd82e0faff88a2e5
Tsai-Chieh <d0177381@gmail.com>
2022-09-20 22:26:31 +0800

    :tada: init banner styled-components

---
+++ src/styled/banner.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+const banner = require('../asset/img/banner-bg.png')
+
+export const Section = styled.section`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${banner});
+  background-position: top center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`
