f3e477aa29496481fe3dbc3479a5fe4f28583af5
Tsai-Chieh <d0177381@gmail.com>
2022-09-20 22:27:01 +0800

    :tada: init navbar styled-components

---
+++ src/styled/navbar.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+const logo = require('../asset/img/logo.png')
+
+export const Nav = styled.nav`
+  position: fixed;
+`
+export const Brand = styled.a``
+export const Logo = styled.img`
+  content: url(${logo});
+  width: 150px;
+`
