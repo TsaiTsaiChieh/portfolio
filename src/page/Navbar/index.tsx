d9d35ba11fc31eef2eae1691f17363c9a5fff07b
Tsai-Chieh <d0177381@gmail.com>
2022-09-21 22:28:26 +0800

    :sparkles: format and add Hamburger component

--- src/page/Navbar/index.tsx
+++ src/page/Navbar/index.tsx
@@ -1,22 +1,29 @@
+import {useState} from 'react'
+
 import {NavbarHref, SocialHref} from '../../constant/Links'
 import {Container} from '../../styled/base'
 import {
-  Brand, Collapse, IconContainer, LangBtn, Logo, Nav, NavContainer, NavLink,
-  SocialContainer, SocialIcon, SocialLink,
+  Brand, Collapse, IconContainer, LangBtn, Logo,
+  Nav, NavContainer, NavLink, SocialContainer,
+  SocialIcon, SocialLink,
 } from '../../styled/navbar'
+import Hamburger from './Hamburger'
 
 const Navbar = () => {
+  const [expanded, setExpanded] = useState<boolean>(false)
+
   return (
     <Nav>
       <Container>
         <Brand href={NavbarHref.home}>
           <Logo />
         </Brand>
-        <Collapse>
+        <Hamburger expanded={expanded} setExpanded={setExpanded} />
+        <Collapse expanded={expanded}>
           <NavContainer>
-            {Object.keys(NavbarHref).slice(1).map((ele, idx) => {
-              return <NavLink key={idx} href={NavbarHref[ele]}>{ele}</NavLink>
-            })}
+            {Object.keys(NavbarHref).slice(1).map((ele, idx) => (
+              <NavLink key={idx} href={NavbarHref[ele]}>{ele}</NavLink>
+            ))}
           </NavContainer>
           <SocialContainer>
             <IconContainer>
