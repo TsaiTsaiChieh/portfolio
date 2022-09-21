ab635b0ad8342acf14862653e278949eb507d1a4
Tsai-Chieh <d0177381@gmail.com>
2022-09-21 16:53:44 +0800

    :tada: init const Mixin

---
+++ src/constant/Mixin.ts
@@ -0,0 +1,11 @@
+const sm: number = 576
+const md: number = 768
+const lg: number = 992
+const xl: number = 1200
+const xxl: number = 1320
+
+export const XXL = `@media all and (min-width: ${xxl}px)`
+export const XL = `@media all and (min-width: ${xl}px)`
+export const LG = `@media all and (min-width: ${lg}px)`
+export const MD = `@media all and (min-width: ${md}px)`
+export const SM = `@media all and (max-width: ${sm}px)`
