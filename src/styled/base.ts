fbc76345e2012440e3ff44a6b1da074ffe3ee9e4
Tsai-Chieh <d0177381@gmail.com>
2022-09-21 16:59:12 +0800

    :iphone: add RWD design for container

--- src/styled/base.ts
+++ src/styled/base.ts
@@ -1,7 +1,28 @@
 import styled from 'styled-components'
 
+import {LG, MD, SM, XL, XXL} from '../constant/Mixin'
+
 export const Container = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  margin-right: auto;
+  margin-left: auto;
+  justify-content: flex-start;
+  flex-wrap: inherit;
+  ${SM} {
+    max-width: 540px;
+  }
+  ${MD} {
+    max-width: 720px;
+  }
+  ${LG} {
+    max-width: 960px;
+  }
+  ${XL} {
+    max-width: 1140px;
+  }
+  ${XXL} {
+    max-width: 1320px;
+  }
 `
