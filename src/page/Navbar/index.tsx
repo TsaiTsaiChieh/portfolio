16bbe99ea4eb871dc24c5c7cd8bafb5c1b57f31d
Tsai-Chieh <d0177381@gmail.com>
2022-09-21 16:55:12 +0800

    :sparkles: import more components

--- src/page/Navbar/index.tsx
+++ src/page/Navbar/index.tsx
@@ -1,13 +1,33 @@
+import {NavbarHref, SocialHref} from '../../constant/Links'
 import {Container} from '../../styled/base'
-import {Brand, Logo, Nav} from '../../styled/navbar'
+import {
+  Brand, Collapse, IconContainer, LangBtn, Logo, Nav, NavContainer, NavLink,
+  SocialContainer, SocialIcon, SocialLink,
+} from '../../styled/navbar'
 
 const Navbar = () => {
   return (
     <Nav>
       <Container>
-        <Brand href='#home'>
+        <Brand href={NavbarHref.home}>
           <Logo />
         </Brand>
+        <Collapse>
+          <NavContainer>
+            {Object.keys(NavbarHref).slice(1).map((ele, idx) => {
+              return <NavLink key={idx} href={NavbarHref[ele]}>{ele}</NavLink>
+            })}
+          </NavContainer>
+          <SocialContainer>
+            <IconContainer>
+              {Object.keys(SocialHref).map((ele, idx) => (
+                <SocialLink key={idx} href={SocialHref[ele]}>
+                  <SocialIcon className={`icon${idx + 1}`} />
+                </SocialLink>))}
+            </IconContainer>
+            <LangBtn>Chinese</LangBtn>
+          </SocialContainer>
+        </Collapse>
       </Container>
     </Nav>
   )
