1fea27dbbd9805574785a82ef810e7a2cc6b1581
Tsai-Chieh <d0177381@gmail.com>
2022-09-21 16:53:25 +0800

    :tada: init const Links

---
+++ src/constant/Links.ts
@@ -0,0 +1,11 @@
+export const NavbarHref: {[index: string]: string} = {
+  home: '#home',
+  skills: '#skills',
+  experience: '#experience',
+  projects: '#projects',
+}
+export const SocialHref: {[index: string]: string} = {
+  linkedIn: 'https://www.linkedin.com/in/%E9%87%87%E6%BD%94-%E8%94%A1-211aa6151',
+  cakeResume: 'https://www.cakeresume.com/tsaitsaichieh',
+  github: 'https://github.com/TsaiTsaiChieh',
+}
