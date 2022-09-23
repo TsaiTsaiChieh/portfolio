237def281c2ec3c2e3de4774b3b0e935ccc09d84
Tsai-Chieh <d0177381@gmail.com>
2022-09-24 00:50:32 +0800

    :sparkles: import Detail and Astronaut component

--- src/page/Banner/index.tsx
+++ src/page/Banner/index.tsx
@@ -1,9 +1,13 @@
+import Lottie from 'lottie-react'
 import {useTranslation} from 'react-i18next'
+import Typewriter from 'typewriter-effect'
 
-import {namespaces} from '../../i18n/constants'
-import {Astronaut, Description, Row, Section, TagLine}
-  from '../../styled/banner'
-import {Container} from '../../styled/base'
+import astronaut from '../../asset/img/astronaut-operating-laptop.json'
+import {namespaces as ns} from '../../i18n/constants'
+import {
+  Astronaut, Box, Description, Details, DownloadCV, Planet, Section, TagLine,
+} from '../../styled/banner'
+import {Container, Row} from '../../styled/base'
 
 const Banner = () => {
   const {t} = useTranslation()
@@ -13,9 +17,26 @@ const Banner = () => {
       <Container>
         <Row>
           <Description>
-            <TagLine>{t('welcome', {ns: namespaces.title})}</TagLine>
+            <TagLine>{t('welcome', {ns: ns.title})}</TagLine>
+            <Typewriter
+              options={{
+                strings: [
+                  t('fullStack', {ns: ns.title}),
+                  t('cleanCode', {ns: ns.title}),
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+            <Details>{t('introduction', {ns: ns.content})}</Details>
+            <Box>
+              <DownloadCV>{t('downloadCV', {ns: ns.button})}</DownloadCV>
+            </Box>
           </Description>
-          <Astronaut />
+          <Astronaut>
+            <Planet />
+            <Lottie className='lottie' animationData={astronaut} loop={true} />
+          </Astronaut>
         </Row>
       </Container>
     </Section>
