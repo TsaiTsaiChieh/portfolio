edcd1615e96f385091380dae5886d58f860b5583
Tsai-Chieh <d0177381@gmail.com>
2022-09-23 14:21:54 +0800

    :sparkles: add more styled-components and translation

--- src/page/Banner/index.tsx
+++ src/page/Banner/index.tsx
@@ -1,8 +1,23 @@
-import {Section} from '../../styled/banner'
+import {useTranslation} from 'react-i18next'
+
+import {namespaces} from '../../i18n/constants'
+import {Astronaut, Description, Row, Section, TagLine}
+  from '../../styled/banner'
+import {Container} from '../../styled/base'
 
 const Banner = () => {
+  const {t} = useTranslation()
+
   return (
-    <Section>
+    <Section id='home'>
+      <Container>
+        <Row>
+          <Description>
+            <TagLine>{t('welcome', {ns: namespaces.title})}</TagLine>
+          </Description>
+          <Astronaut />
+        </Row>
+      </Container>
     </Section>
   )
 }
