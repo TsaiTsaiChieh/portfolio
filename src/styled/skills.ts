c24e07abdf87bc684c608bad5a59e1f658069e07
Tsai-Chieh <d0177381@gmail.com>
2022-09-25 18:09:42 +0800

    :tada: init Skills component

---
+++ src/styled/skills.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+// import background from '../asset/img/color-sharp1.png'
+import {colors} from '../constant/Variables'
+
+export const Section = styled.section`
+  height: 100vh;
+  background-color: ${colors.black[50]};
+`
