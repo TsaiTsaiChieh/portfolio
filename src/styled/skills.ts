7153ad2bb2572839e172eb8a751351f34190f6ff
Tsai-Chieh <d0177381@gmail.com>
2022-09-25 23:13:14 +0800

    :lipstick: update skill styled-components

--- src/styled/skills.ts
+++ src/styled/skills.ts
@@ -1,9 +1,103 @@
 import styled from 'styled-components'
 
-// import background from '../asset/img/color-sharp1.png'
+import background from '../asset/img/color-sharp2.png'
+import {LG} from '../constant/Mixin'
 import {colors} from '../constant/Variables'
 
 export const Section = styled.section`
-  height: 100vh;
-  background-color: ${colors.black[50]};
+  height: auto;
+  background: ${colors.black[50]};
+  display: flex;
+  position: relative;
+  overflow: hidden;
+  ${LG} {
+    height: 100vh;
+  }
+`
+export const Title = styled.h2`
+  width: 100%;
+  font-size: 45px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: center;
+  margin-bottom: 40px;
+  &::first-letter {
+    text-transform: uppercase;
+  }
+`
+export const CardContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  justify-content: space-evenly;
+  z-index: 10;
+  ${LG} {
+    flex-direction: row;
+  }
+`
+export const CardBody = styled.li`
+  width: 350px;
+  height: 552px;
+  position: absolute;
+  border-radius: 10px;
+  inset: 2px;
+  align-items: center;
+  background: ${colors.black[60]};
+  z-index: 10;
+ 
+`
+export const SkillGroup = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+`
+export const SkillItem = styled.li`
+  display: flex;
+  align-items: center;
+  svg {
+    color: black;
+    padding: 15px;
+    font-size: 80px;
+    border-radius: 50%;
+    background: white;
+  }
+  span {
+    font-size: 18px;
+    margin-left: 15px;
+    letter-spacing: 1px;
+  }
+`
+export const CardBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 0 0 10px 10px;
+  &.backend {
+    background: ${colors.primary['opacity-200']};
+  }
+  &.frontend {
+    background: ${colors.primary['opacity-300']};
+  }
+  &.devOps {
+    background: ${colors.primary['opacity-400']};
+  }
+`
+export const CardLabel = styled.label`
+  font-size: 24px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`
+export const CardDetails = styled.p`
+  line-height: 1.2rem;
+  color: ${colors.black[100]};
+`
+export const Background = styled.img`
+  content: url(${background});
+  top: -20%;
+  left: 0;
+  position: absolute;
+  z-index: 0;
 `
