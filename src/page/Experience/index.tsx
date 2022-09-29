35fe3beaf6716efcba8ee5bdad5a5b506a04b2ec
Tsai-Chieh <d0177381@gmail.com>
2022-09-29 22:56:12 +0800

    :tada: init experience component

--- src/page/Experience/index.tsx
+++ src/page/Experience/index.tsx
@@ -1,7 +1,42 @@
-import {Section} from '../../styled/experience'
+import {useTranslation} from 'react-i18next'
+
+import everFortune from '../../asset/img/everFortune.png'
+import {NavbarHref} from '../../constant/Links'
+import {Col, Container} from '../../styled/base'
+import {Background, Section, Title} from '../../styled/experience'
+import Timeline from './Timeline'
 
 const Experience = () => {
-  return <Section id='experience'>Experience</Section>
+  const {t} = useTranslation()
+  const Milestones = [{
+    jobTitle: t('milestone.5.jobTitle'),
+    company: t('milestone.5.company'),
+    interval: t('milestone.5.interval'),
+    icon: everFortune,
+    mainTask: t('milestone.5.mainTask'),
+    tasks: t('milestone.5.tasks', {returnObjects: true}),
+  },
+  {
+    jobTitle: t('milestone.4.jobTitle'),
+    company: t('milestone.4.company'),
+    interval: t('milestone.4.interval'),
+    icon: everFortune,
+    mainTask: t('milestone.4.mainTask'),
+    tasks: t('milestone.4.tasks', {returnObjects: true}),
+  },
+  ]
+
+  return <Section id={Object.keys(NavbarHref)[2]}>
+    <Container>
+      <Col>
+        <Title>{t('nav.experience')}</Title>
+        {Milestones.map((ele, i) => (
+          <Timeline key={i} milestone={ele} />
+        ))}
+      </Col>
+    </Container>
+    <Background />
+  </Section>
 }
 
 export default Experience
