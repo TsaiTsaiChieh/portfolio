9d4a6bac2f60c0dc80a13f21688543677ff94d7f
Tsai-Chieh <d0177381@gmail.com>
2022-10-03 00:18:02 +0800

    :pencil2: add SkillCategory

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,7 @@ declare module '*.jpg'
 declare module '*.svg'
 declare module '*.png'
 
+type SkillCategory = 'backend' | 'frontend' | 'devOps'
 type SkillTreeType = {
   name: string
   learnings: string[]
