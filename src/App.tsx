72922df6ed463ac7aa32dba7f71018f03f2926b3
Tsai-Chieh <d0177381@gmail.com>
2022-10-06 23:43:46 +0800

    :sparkles: import Launch component for loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css'
 
+import {useState} from 'react'
+
+import Launch from './components/Launch'
 import {Banner, Experience, Navbar, Skills} from './page'
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(true)
+
   return (
-    <>
+    loading ? (<Launch loading={loading} setLoading={setLoading} />) : (<>
       <Navbar />
       <Banner />
       <Skills />
       <Experience />
-    </>
+    </>)
   )
 }
 
