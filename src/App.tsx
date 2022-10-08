8fda63e68f9d9ad854565f8f2a8abc36616809f0
Tsai-Chieh <d0177381@gmail.com>
2022-10-08 17:16:20 +0800

    :fire: rm loading lottie

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import './App.css'
 
-import {useState} from 'react'
-
-import Launch from './components/Launch'
 import {Banner, Experience, Navbar, Skills} from './page'
 
 const App = () => {
-  const [loading, setLoading] = useState<boolean>(true)
-
   return (
-    loading ? (<Launch loading={loading} setLoading={setLoading} />) : (<>
+    <>
       <Navbar />
       <Banner />
       <Skills />
       <Experience />
-    </>)
+    </>
   )
 }
 
