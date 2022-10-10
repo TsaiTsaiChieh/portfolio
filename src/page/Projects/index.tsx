a9a82b2ca0ed24cecc5ddb8da3431ed958f1b0d1
Tsai-Chieh <d0177381@gmail.com>
2022-10-11 07:14:11 +0800

    :sparkles: import Tab component

--- src/page/Projects/index.tsx
+++ src/page/Projects/index.tsx
@@ -1,8 +1,30 @@
+import {useState} from 'react'
+import {useTranslation} from 'react-i18next'
+
+import {Col, Container, Title} from '../../styled/base'
 import {Section} from '../../styled/projects'
+import Tab from './Tab'
 
 const Projects = () => {
+  const {t} = useTranslation()
+  const ProjectItems: ProjectItemType[] = t('projects', {
+    returnObjects: true,
+  })
+  const [active, setActive] = useState<string>(ProjectItems[0].name)
+
   return (
-    <Section id='projects' />
+    <Section id='projects'>
+      <Container>
+        <Col>
+          <Title>{t('nav.projects')}</Title>
+          <Tab
+            ProjectItems={ProjectItems}
+            active={active}
+            setActive={setActive}
+          />
+        </Col>
+      </Container>
+    </Section>
   )
 }
 
