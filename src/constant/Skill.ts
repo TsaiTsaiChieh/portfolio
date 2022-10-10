daae935e30266571370108dcdb259012e6bc0db7
Tsai-Chieh <d0177381@gmail.com>
2022-10-10 17:18:52 +0800

    :sparkles: add skill const

---
+++ src/constant/Skill.ts
@@ -0,0 +1,18 @@
+export const SkillTags: {[skills in SkillCategory]: string[]} = {
+  backend: [
+    'NodeJS',
+    'Python',
+    'MySQL',
+    'Web Crawler',
+    'PostgreSQL',
+    'Firebase',
+    'NoSQL',
+  ],
+  frontend: ['React', 'Redux', 'Typescript', 'HTML/CSS'],
+  devOps: ['AWS', 'GCP', 'Docker', 'PM2'],
+}
+export const Skills: {[skills in SkillCategory]: string[]} = {
+  backend: ['NodeJS', 'Python', 'MySQL', 'Web Crawler'],
+  frontend: ['React', 'Redux Toolkit', 'Typescript', 'HTML/CSS'],
+  devOps: ['AWS', 'GCP', 'Docker', 'PM2'],
+}
