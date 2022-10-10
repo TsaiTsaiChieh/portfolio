d25cbea4eaa68aa0f100c5aa4241e9eb48c58c99
Tsai-Chieh <d0177381@gmail.com>
2022-10-10 17:19:15 +0800

    :sparkles: add tagColor func

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,4 +1,17 @@
+import {SkillTags} from '../constant/Skill'
+import {colors} from '../constant/Variables'
+
 export const aTag = (arr: string[]): JSX.Element[] =>
   arr.map((ele, idx) => (
     <a key={idx + 1} href={ele} target='_blank' rel='noopener noreferrer' />
   ))
+export const tagColor = (val: string) => {
+  switch (true) {
+    case SkillTags.backend.includes(val):
+      return colors.primary[200]
+    case SkillTags.frontend.includes(val):
+      return colors.primary[300]
+    default:
+      return colors.primary[400]
+  }
+}
