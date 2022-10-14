98e347bdb7aa73dc78f934aa55f14b32f3c90c82
Tsai-Chieh <d0177381@gmail.com>
2022-10-14 21:42:37 +0800

    :sparkles: add i optional input in aTag func

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,9 +1,14 @@
 import {SkillTags} from '../constant/Skill'
 import {colors} from '../constant/Variables'
 
-export const aTag = (arr: string[]): JSX.Element[] =>
+export const aTag = (arr: string[], i?: number): JSX.Element[] =>
   arr.map((ele, idx) => (
-    <a key={idx + 1} href={ele} target='_blank' rel='noopener noreferrer' />
+    <a
+      key={i ? i : idx + 1}
+      href={ele}
+      target='_blank'
+      rel='noopener noreferrer'
+    />
   ))
 export const tagColor = (val: string) => {
   switch (true) {
