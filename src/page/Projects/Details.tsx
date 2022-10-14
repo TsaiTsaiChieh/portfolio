42c766b62d4186f5c41f5aa881a9a7d1fdb4f8d9
Tsai-Chieh <d0177381@gmail.com>
2022-10-14 22:44:19 +0800

    :tada: init Details component

---
+++ src/page/Projects/Details.tsx
@@ -0,0 +1,52 @@
+import {Trans} from 'react-i18next'
+
+import {Modifier} from '../../constant/Enum'
+import {ProjectsHref} from '../../constant/Links'
+import {LinkIcon, SkillTag} from '../../styled/base'
+import {
+  DetailsWrap, IconWrap, InfoWrap, ProjectName, Summary, TagWrap,
+} from '../../styled/projects'
+import {aTag, tagColor} from '../../utils/helper'
+
+interface Props {
+  idx: number
+  project: ProjectItemType
+  github: string
+}
+const Details = ({idx, project, github}: Props) => {
+  const {summary, name, tags, spend} = project
+
+  return (
+    <DetailsWrap>
+      <ProjectName>{name}</ProjectName>
+      <Summary>{summary}</Summary>
+      <TagWrap>
+        {tags.map((ele, i) => (
+          <SkillTag key={i} color={tagColor(ele)}>
+            {ele}
+          </SkillTag>
+        ))}
+      </TagWrap>
+      <InfoWrap>
+        <span>{spend}</span>
+        <IconWrap
+          className={ProjectsHref[idx] === '' ? Modifier.DEPRECATED : ''}
+        >
+          <Trans
+            i18nKey={`projects.${idx}.website`}
+            components={aTag([ProjectsHref[idx]], idx)}
+          />
+          {ProjectsHref[idx] === '' ? null : <LinkIcon />}
+        </IconWrap>
+        <IconWrap>
+          <a href={github} target='_blank' rel='noopener noreferrer'>
+            Github
+          </a>
+          <LinkIcon />
+        </IconWrap>
+      </InfoWrap>
+    </DetailsWrap>
+  )
+}
+
+export default Details
