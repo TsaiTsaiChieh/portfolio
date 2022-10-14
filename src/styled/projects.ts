07a725d5b14d8789ea9515c511bfa1375a0dc3de
Tsai-Chieh <d0177381@gmail.com>
2022-10-14 22:43:55 +0800

    :sparkles: update projects styled-component

--- src/styled/projects.ts
+++ src/styled/projects.ts
@@ -15,6 +15,7 @@ export const BtnWrap = styled.div`
   border-radius: 50px;
   font-size: 14px;
   justify-content: space-around;
+  margin-bottom: 60px;
   ${MD} {
     width: 75%;
     font-size: 16px;
@@ -34,7 +35,7 @@ export const Btn = styled.button`
   position: relative;
   z-index: 0;
   overflow: hidden;
-
+  opacity: 0.75;
   &:first-child {
     border-radius: 50px 0 0 50px;
     border: 1px solid ${colors.black['opacity-50']};
@@ -61,10 +62,74 @@ export const Btn = styled.button`
   &.active::before {
     width: 100%;
   }
+  &.active,
+  &:hover {
+    opacity: 1;
+  }
 `
-export const SideProjectWrap = styled.div`
+export const DetailsWrap = styled.div`
+  width: 55%;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  line-height: 1.3rem;
+  margin: auto;
+  text-align: center;
+  margin-bottom: 30px;
+`
+export const ImgWrap = styled.div`
+width: 100%;
+height:400px;
+margin:auto ;
+text-align:center;
+img {
+  height:auto;
+  border-radius: 10px;
+}
+${MD} {
+  width: 55%;
+}
+`
+export const ProjectImg = styled.img<{$path: string}>`
+  content: url(${(props) => props.$path});
+`
+
+export const ProjectName = styled.h1`
+  font-size: 22px;
+  color: white;
+  margin-bottom: 20px;
+  font-weight:bold;
+letter-spacing:.5px ;
+`
+export const Summary = styled.span``
+export const TagWrap = styled.ul`
+  flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin: 15px 0;
+`
+export const SkillTag = styled.li<{color: string}>`
+background: ${(props) => props.color};
+  font-style: italic;
+  font-size: 12px;
+  padding: 0px 5px;
+  border-radius: 10px;
+  &::before {
+    content: "#";
+  }
+`
+export const InfoWrap = styled.div`
 display: flex;
+justify-content: center;
+gap: 20px;
 `
-export const Screenshot = styled.img<{$path: string}>`
-content: url(${(props) => props.$path})
+export const IconWrap = styled.div`
+font-size: 16px;
+a {
+  text-decoration:underline;
+}
+&.deprecated {
+  color: ${colors.black[100]};
+}
 `
