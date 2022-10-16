12d96e30490c185eb02d9f1a16e5dd6520109968
Tsai-Chieh <d0177381@gmail.com>
2022-10-16 19:43:37 +0800

    :sparkles: finish footer component

--- src/page/Footer/index.tsx
+++ src/page/Footer/index.tsx
@@ -1,6 +1,28 @@
+import {SocialHref} from '../../constant/Links'
+import {Container} from '../../styled/base'
+import {IconContainer, Section} from '../../styled/footer'
+import {
+  Logo,
+  SocialIcon,
+  SocialLink,
+} from '../../styled/navbar'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Section id='footer'>
+      <Container>
+        <Logo />
+        <IconContainer>
+          {Object.keys(SocialHref).map((ele, i) => (
+            <SocialLink key={i} href={SocialHref[ele]}>
+              <SocialIcon className={`icon${i + 1}`} />
+            </SocialLink>
+          ))}
+          <span className='copyright'>CopyRight 2022. All Right Reserved</span>
+          <span className='email'>jecica196@gmail.com</span>
+        </IconContainer>
+      </Container>
+    </Section>
   )
 }
 
